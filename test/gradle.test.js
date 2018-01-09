69a098ee3b9f8747d9c8ffe90fad54eac5b9d7d0
xdf <xudafeng@126.com>
2018-01-09 15:12:53 +0800

    update deps (#1)
    * update deps

--- test/gradle.test.js
+++ test/gradle.test.js
@@ -1,7 +1,20 @@
 'use strict';
 
-var gradle = require('..');
+const gradle = require('..');
+const assert = require('assert');
 
-gradle({
-  cwd: process.cwd()
+describe('test', () => {
+  it('should be ok', () => {
+    assert.ok(gradle);
+    gradle({
+      cwd: process.cwd()
+    })
+      .then(d => {
+        console.log(d);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  });
 });
+
